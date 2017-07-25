9246000558b5e1be32496590a66d279aa630a18c
Alexey Malyshev <alexey.malyshev@hotmail.com>
2017-07-25 17:02:04 +0300

    Pixel test added

---
+++ MidiBotTesting/BmpToDisplayTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace MidiBotTesting
+{
+    [TestClass]
+    public class BmpToDisplayTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            byte r, g, b;
+            r = 1;
+            g = 10;
+            b = 10;
+            int pixel = b >> 3;
+            pixel <<= 6;
+            pixel += g >> 2;
+            pixel <<= 5;
+            pixel += r >> 3;
+            Trace.WriteLine(Convert.ToString(pixel, 2));
+        }
+    }
+}
---
+++ MidiBotTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MidiBotTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Aon")]
+[assembly: AssemblyProduct("MidiBotTesting")]
+[assembly: AssemblyCopyright("Copyright © Aon 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ae14027a-a577-4ace-b349-5c7186037c39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
